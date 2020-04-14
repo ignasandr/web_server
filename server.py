51a32b29d319ec9d73d3b760c8433cbe28cb7537
ignasandr <63236361+ignasandr@users.noreply.github.com>
2020-04-14 22:21:05 +0300

    Basic server

--- server.py
+++ server.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 print(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/<username>')
+def custom(username=None):
+    return render_template('custom.html', name=username)
