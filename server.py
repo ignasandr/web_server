38855c7964d1efd9e7ddba9982a357b9a0b5541c
ignasandr <63236361+ignasandr@users.noreply.github.com>
2020-04-15 09:25:06 +0300

    Template live, form data to txt file

--- server.py
+++ server.py
@@ -1,18 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 print(__name__)
 
 
 @app.route('/')
-def index():
+def my_home():
     return render_template('index.html')
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/<string:path>')
+def my_index(path):
+    return render_template(path)
 
 
-@app.route('/<username>')
-def custom(username=None):
-    return render_template('custom.html', name=username)
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n {email}, {subject}, {message}')
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_file(data)
+        return redirect('/thank_you.html')
+    else:
+        return 'something went wrong. Try again'
